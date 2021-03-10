f2bbd405f2b3157b013b82604f4cc68f57860959
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-10 21:49:30 +0000

    Initial commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,41 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// PAIR PROGRAMMING ACTIVITY - WORD SEARCH
+// Reggi & Jared
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  return horizontalJoin(letters, word) || horizontalJoin(transpose(letters), word);
+};
+
+const horizontalJoin = (matrix, word) => {
+  const searchWord = word ? word.split(' ').join('') : '';
+  const joinedArray = matrix.map(ls => ls.join(''));
+  if (word) {
+    for (let l of joinedArray) {
+      if (l.includes(searchWord)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const transpose = function(matrix) {
+  // Initialize a new array
+  const transposedMatrix = [];
+  // Get number of columns
+  const columns = matrix[0].length;
+  // For each column index make new row
+  for (let i = 0; i < columns; i++) {
+    const newRow = [];
+    // For each row in original
+    for (let row in matrix) {
+      // Grab element at current column index and add to new row
+      newRow.push(matrix[row][i]);
+    }
+    // Add new row to grid
+    transposedMatrix.push(newRow);
+  }
+  // Return resulting grid
+  return transposedMatrix;
+};
+
+module.exports = wordSearch;
